58eacc858fdc07114a891f4d5ab9d9d2d2142bd2
hesam <m.hesam.moradian@gmail.com>
2023-10-29 19:14:42 +0330

    marketComponent almost done

--- src/Components/marketComponent/marketComponent.js
+++ src/Components/marketComponent/marketComponent.js
@@ -4,82 +4,33 @@ import React, { useState } from "react";
 import Slider from "@mui/material/Slider";
 import Typography from "@mui/material/Typography";
 import Dropdown from "react-bootstrap/Dropdown";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
-function ProductCategory() {
+
+function DropDown({ title, items }) {
   return (
     <Dropdown>
       <Dropdown.Toggle
         id="dropdown-basic"
-        className="w-100 bg-white text-secondary  border-secondary fs-4  d-flex align-items-center gap-3 rounded-4"
+        className="w-100 bg-white text-secondary  border fs-4  d-flex align-items-center gap-3 rounded-4"
       >
-        دسته بندی محصولات
+        {title}
       </Dropdown.Toggle>
 
-      <Dropdown.Menu className="w-100 fs-5 ">
-        <Dropdown.Item
-          href="#/action-1 "
-          className="d-flex justify-content-start "
-        >
-          انواع کارت حافظه
-        </Dropdown.Item>
-        <Dropdown.Item
-          href="#/action-2"
-          className="d-flex justify-content-start "
-        >
-          تجهیزات شبکه
-        </Dropdown.Item>
-        <Dropdown.Item
-          href="#/action-3"
-          className="d-flex justify-content-start "
-        >
-          کالای دیجیتال
-        </Dropdown.Item>
-        <Dropdown.Item
-          href="#/action-3"
-          className="d-flex justify-content-start "
-        >
-          {" "}
-          کامپیوتر و لوازم جانبی
-        </Dropdown.Item>
-        <Dropdown.Item
-          href="#/action-3"
-          className="d-flex justify-content-start "
-        >
-          هدفون، هدست
-        </Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-  );
-}
-function ProductBrand() {
-  return (
-    <Dropdown>
-      <Dropdown.Toggle
-        id="dropdown-basic"
-        className="w-100 bg-white text-secondary e border-secondary fs-4 d-flex align-items-center gap-3 rounded-4 "
-      >
-        برند محصولات
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu className="w-100 fs-5">
-        <Dropdown.Item
-          href="#/action-1"
-          className="d-flex justify-content-start "
-        >
-          kaku
-        </Dropdown.Item>
-        <Dropdown.Item
-          href="#/action-2"
-          className="d-flex justify-content-start "
-        >
-          rapoo
-        </Dropdown.Item>
-        <Dropdown.Item
-          href="#/action-3"
-          className="d-flex justify-content-start "
-        >
-          silicon power
-        </Dropdown.Item>
+      <Dropdown.Menu className=" rounded-4 bg-light border-0 shadow fs-5 ">
+        {items.map((element) => {
+          // {console.log(element)}
+          return (
+            <Dropdown.Item
+              href="#/action-1 "
+              className="d-flex justify-content-start "
+            >
+              {console.log(element)}
+              {element}
+            </Dropdown.Item>
+          );
+        })}
       </Dropdown.Menu>
     </Dropdown>
   );
@@ -93,7 +44,7 @@ const DoubleSlider = () => {
   };
 
   return (
-    <div>
+    <div className=" border   rounded-4  p-5">
       {/* <Typography id="range-slider" gutterBottom></Typography> */}
       <span> فیلتر قیمت</span>
       <Slider
@@ -115,6 +66,56 @@ const DoubleSlider = () => {
   );
 };
 
+function ProductCard() {
+  return (
+    <Card
+      className="ProductCard w-100 d-flex align-items-center rounded-5 col border-0 m-3 shadow-sm "
+      style={{ width: "18rem" }}
+    >
+      {/* <span className="topTitle p-4 text-secondary ">
+          تخفیف های ویژه{" "}
+          <span className="topTitleBold fw-bolder text-danger ">امروز</span>
+        </span> */}
+      <Card.Img
+        className="w-50 mt-5"
+        variant="top"
+        src="https://halochin.ir/electronic-shop/wp-content/uploads/2023/08/product-image-2.jpg"
+      />
+      <Card.Body className="w-100 ">
+        <Card.Title className="mx-4">
+          رم دسکتاپ DDR4 تک کاناله 2666 مگاهرتز کروشیال ظرفیت 8 گیگابایت
+        </Card.Title>
+        <hr />
+        <Card.Text className="d-flex justify-content-between flex-row-reverse p-4">
+          <div className="cardTextContainer">
+            <div className="SliderPrice d-grid ">
+              <span className="SliderPrice-discont small fw-light text-decoration-line-through text-danger ">
+                880,000
+              </span>
+              <div className="SliderPrice-mainPriceContainer">
+                <span className="SliderPrice-price p-1 fw-bolder ">
+                  {" "}
+                  780,000
+                </span>
+                <span className="SliderPrice-toman text-secondary  ">
+                  تومان{" "}
+                </span>
+              </div>
+            </div>
+          </div>
+          <Button
+            variant="white"
+            className="ProductCardBTN border-secondary rounded-4  "
+          >
+            <span class="ProductCardBTNSpan material-symbols-outlined p-1 text-secondary ">
+              shopping_bag
+            </span>
+          </Button>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
 export default function marketComponent() {
   return (
     <div className="row">
@@ -131,8 +132,21 @@ export default function marketComponent() {
           جستجو کنید
         </a>
         <DoubleSlider />
-        <ProductCategory />
-        <ProductBrand />
+        <DropDown
+          title="دسته بندی محصولات"
+          items={[
+            "انواع کارت حافظه",
+            "تجهیزات شبکه",
+            "کالای دیجیتال",
+            "کامپیوتر و لوازم جانبی",
+            "هدفون، هدست",
+          ]}
+        />
+        <DropDown
+          title="برند محصولات"
+          items={["kaku", "rapoo", "silicon power", "verity", "تسکو"]}
+        />
+
         <a
           class="btn btn-sm w-100 btn-color fs-4 p-3 rounded-4"
           href="#"
@@ -142,9 +156,26 @@ export default function marketComponent() {
           اعمال فیلتر ها{" "}
         </a>
       </div>
-      <div class="col p-5">
-
-        
+      <div class="col p-5 marketComponentProducts">
+        <header className="d-flex justify-content-between border p-5 rounded-5">
+          <h2 className="fs-2 fw-bold text-dark opacity-75 ">فروشگاه</h2>
+          <div className="">
+            <DropDown
+              title="مرتب سازی براساس ..."
+              items={[
+                "براساس قیمت از ارزان ترین به گران ترین",
+                "براساس قیمت از گران ترین به ارزان ترین",
+                "براساس تاریخ از قدیمی ترین به جدیدترین",
+                "براساس تاریخ از جدیدترین به قدیمی ترین",
+              ]}
+            />
+          </div>
+        </header>
+        <div className="marketComponentProductContainer row m-0">
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </div>
       </div>
     </div>
   );
