e5d5236a08796be7f7d2651b57d943d4e47c96e7
hesam <m.hesam.moradian@gmail.com>
2023-10-31 10:50:21 +0330

    start BlogPageNewArtickles

---
+++ src/Components/BlogPageNewArtickles/BlogPageNewArtickles.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./BlogPageNewArtickles.css";
+
+export default function BlogPageNewArtickles() {
+  return (
+    <div>
+      <div>sss</div>
+    </div>
+  );
+}
--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -1,12 +1,13 @@
 import React from "react";
 import BlogHeader from "../../Components/BlogHeader/BlogHeader";
+import BlogPageNewArtickles from "../../Components/BlogPageNewArtickles/BlogPageNewArtickles";
 import "./Blog.css";
 
 export default function Blog() {
   return (
-      <div>
-          <BlogHeader/>
-
+    <div>
+      <BlogHeader />
+      <BlogPageNewArtickles />
     </div>
   );
 }
