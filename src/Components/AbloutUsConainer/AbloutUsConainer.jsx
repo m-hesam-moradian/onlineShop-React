31dbed128c089f116bef36165476e124aa56495e
hesam <m.hesam.moradian@gmail.com>
2023-11-02 22:01:37 +0330

    .

--- src/Components/AbloutUsConainer/AbloutUsConainer.jsx
+++ src/Components/AbloutUsConainer/AbloutUsConainer.jsx
@@ -77,6 +77,7 @@ export default function AbloutUsConainer() {
           />
         </div>
       </div>
+      
 
       {/*  ///////////////////////////////*/}
       <div className="spinner p-md-5  row m-0 gap-4 ">
