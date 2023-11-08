bc5e8ef14630acbe86fdd7bc34b3a840628c0ef2
hesam <m.hesam.moradian@gmail.com>
2023-11-08 20:47:23 +0330

    .

--- src/pages/ProductInfo/ProductInfo.jsx
+++ src/pages/ProductInfo/ProductInfo.jsx
@@ -1,18 +1,15 @@
 import { API } from "../../FirebaseDatas";
 import { useEffect } from "react";
 import { useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import Navbar from "../../Components/Navbar/Navbar";
 import Footer from "../../Components/Footer/Footer";
 import Breadcrumb from "react-bootstrap/Breadcrumb";
 import "./ProductInfo.css";
 
 export default function ProductInfo() {
-  const [ProductArray, setProductArray] = useState([]);
   const [ProductData, setProductData] = useState([]);
-  const [searchResult, setsearchResult] = useState([]);
   const { productID } = useParams();
-  const navigate = useNavigate();
 
   useEffect(() => {
     fetch(`${API}products.json`)
@@ -23,17 +20,16 @@ export default function ProductInfo() {
         return res.json();
       })
       .then((allData) => {
- 
         setProductData(
           allData.find((item) => parseInt(item.id) == parseInt(productID))
         );
-    })
-    .catch((error) => {
+      })
+      .catch((error) => {
         console.error("Error fetching data:", error);
       });
   }, []);
 
-  console.log( ProductData);
+  console.log(ProductData);
   {
     console.log();
   }
@@ -41,20 +37,13 @@ export default function ProductInfo() {
     <div>
       <Navbar />
       <Breadcrumb dir="ltr" className="backColor fs-3 m-5 p-2 rounded-4 d-grid">
-        <Breadcrumb.Item  href="#">
-          خانه{" "}
-        </Breadcrumb.Item>
+        <Breadcrumb.Item href="#">خانه </Breadcrumb.Item>
 
-        <Breadcrumb.Item
-          
-          href="https://getbootstrap.com/docs/4.0/components/breadcrumb/"
-        >
+        <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
           کالای دیجیتال
         </Breadcrumb.Item>
 
-        <Breadcrumb.Item  >
-          {ProductData.name}
-        </Breadcrumb.Item>
+        <Breadcrumb.Item>{ProductData.name}</Breadcrumb.Item>
       </Breadcrumb>
       <Footer />
     </div>
