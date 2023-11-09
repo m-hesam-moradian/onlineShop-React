dc101df14d7fe2e0b981e10698c3099db41aa4e4
hesam <m.hesam.moradian@gmail.com>
2023-11-09 17:03:20 +0330

    half login done

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,195 +1,129 @@
-// import React, { useContext, useState } from "react";
-// import { Link } from "react-router-dom";
-// import Footer from "../../Components/Footer/Footer";
-// import Button from "../../Components/Form/Button";
-// import Input from "../../Components/Form/Input";
-// import Navbar from "../../Components/Navbar/Navbar";
-// import Topbar from "../../Components/Topbar/Topbar";
-// import { useForm } from "../../hooks/useForm";
-// import AuthContext from "../../context/authContext";
-// import swal from "sweetalert";
-// import { useNavigate } from "react-router-dom";
-// import ReCAPTCHA from "react-google-recaptcha";
+import React, { useContext, useState } from "react";
+import "./Login.css";
+import Navbar from "../../Components/Navbar/Navbar";
+import Footer from "../../Components/Footer/Footer";
 
-// import {
-//   requiredValidator,
-//   minValidator,
-//   maxValidator,
-//   emailValidator,
-// } from "../../validators/rules";
-
-// import "./Login.css";
-
-// export default function Login() {
-//   const navigate = useNavigate();
-//   const authContext = useContext(AuthContext);
-//   const [isGoogleRecaptchaVerify, setIsGoogleRecaptchaVerify] = useState(false)
-
-//   const [formState, onInputHandler] = useForm(
-//     {
-//       username: {
-//         value: "",
-//         isValid: false,
-//       },
-//       password: {
-//         value: "",
-//         isValid: false,
-//       },
-//     },
-//     false
-//   );
-
-//   const userLogin = (event) => {
-//     event.preventDefault();
-
-//     const userData = {
-//       identifier: formState.inputs.username.value,
-//       password: formState.inputs.password.value,
-//     };
-
-//     fetch(`http://localhost:4000/v1/auth/login`, {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify(userData),
-//     })
-//       .then((res) => {
-//         if (!res.ok) {
-//           return res.text().then((text) => {
-//             throw new Error(text);
-//           });
-//         } else {
-//           return res.json();
-//         }
-//       })
-//       .then((result) => {
-//         swal({
-//           title: "با موفقیت لاگین شدید",
-//           icon: "success",
-//           buttons: "ورود به پنل",
-//         }).then((value) => {
-//           navigate("/");
-//         });
-//         authContext.login({}, result.accessToken);
-//       })
-//       .catch((err) => {
-//         swal({
-//           title: "همچین کاربری وجود ندارد",
-//           icon: "error",
-//           buttons: "تلاش دوباره",
-//         });
-//       });
-//   };
-
-//   const onChangeHandler = () => {
-//     setIsGoogleRecaptchaVerify(true)
-//   }
-
-//   return (
-//     <>
-//       <Topbar />
-//       <Navbar />
-
-//       <section className="login-register">
-//         <div className="login">
-//           <span className="login__title">ورود به حساب کاربری</span>
-//           <span className="login__subtitle">
-//             خوشحالیم دوباره میبینیمت دوست عزیز :)
-//           </span>
-//           <div className="login__new-member">
-//             <span className="login__new-member-text">کاربر جدید هستید؟</span>
-//             <Link className="login__new-member-link" to="/register">
-//               ثبت نام
-//             </Link>
-//           </div>
-//           <form action="#" className="login-form">
-//             <div className="login-form__username">
-//               <Input
-//                 className="login-form__username-input"
-//                 id="username"
-//                 type="text"
-//                 placeholder="نام کاربری یا آدرس ایمیل"
-//                 element="input"
-//                 validations={[
-//                   requiredValidator(),
-//                   minValidator(8),
-//                   maxValidator(20),
-//                   // emailValidator(),
-//                 ]}
-//                 onInputHandler={onInputHandler}
-//               />
-//               <i className="login-form__username-icon fa fa-user"></i>
-//             </div>
-//             <div className="login-form__password">
-//               <Input
-//                 element="input"
-//                 id="password"
-//                 type="password"
-//                 className="login-form__password-input"
-//                 placeholder="رمز عبور"
-//                 validations={[
-//                   requiredValidator(),
-//                   minValidator(8),
-//                   maxValidator(18),
-//                 ]}
-//                 onInputHandler={onInputHandler}
-//               />
-
-//               <i className="login-form__password-icon fa fa-lock-open"></i>
-//             </div>
-//             <div className="login-form__password recaptcha-parent">
-//               <ReCAPTCHA sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI" onChange={onChangeHandler} />,
-//             </div>
-//             <Button
-//               className={`login-form__btn ${
-//                 (formState.isFormValid && isGoogleRecaptchaVerify)
-//                   ? "login-form__btn-success"
-//                   : "login-form__btn-error"
-//               }`}
-//               type="submit"
-//               onClick={userLogin}
-//               disabled={(!formState.isFormValid || !isGoogleRecaptchaVerify)}
-//             >
-//               <i className="login-form__btn-icon fas fa-sign-out-alt"></i>
-//               <span className="login-form__btn-text">ورود</span>
-//             </Button>
-//             <div className="login-form__password-setting">
-//               <label className="login-form__password-remember">
-//                 <input
-//                   className="login-form__password-checkbox"
-//                   type="checkbox"
-//                 />
-//                 <span className="login-form__password-text">
-//                   مرا به خاطر داشته باش
-//                 </span>
-//               </label>
-//               <label className="login-form__password-forget">
-//                 <a className="login-form__password-forget-link" href="#">
-//                   رمز عبور را فراموش کرده اید؟
-//                 </a>
-//               </label>
-//             </div>
-//           </form>
-//           <div className="login__des">
-//             <span className="login__des-title">سلام کاربر محترم:</span>
-//             <ul className="login__des-list">
-//               <li className="login__des-item">
-//                 لطفا از مرورگر های مطمئن و بروز مانند گوگل کروم و فایرفاکس
-//                 استفاده کنید.
-//               </li>
-//               <li className="login__des-item">
-//                 ما هرگز اطلاعات محرمانه شمارا از طریق ایمیل درخواست نمیکنیم.
-//               </li>
-//               <li className="login__des-item">
-//                 لطفا کلمه عبور خود را در فواصل زمانی کوتاه تغییر دهید.
-//               </li>
-//             </ul>
-//           </div>
-//         </div>
-//       </section>
-
-//       <Footer />
-//     </>
-//   );
-// }
+export default function Login() {
+  return (
+    <>
+      <Navbar />
+      <div className="row m-0 LoginPage">
+        <div className="col m-4 ">
+          <span className="mb-4 ">عضویت</span>
+          <div className="bg-light rounded-5  mt-5">
+            <div className="px-5 pt-5">
+              <span className='fs-4 text-secondary'>
+                نام کاربری <span className="text-danger fw-bold  ">*</span>
+              </span>
+              <input
+                type="text"
+                class="form-control fs-4 p-3 rounded-4 border-0 "
+                name=""
+                id=""
+                aria-describedby="helpId"
+               
+              />
+            </div>
+            <div className="px-5 pt-5">
+              <span className='fs-4 text-secondary'>
+                نام کاربری <span className="text-danger fw-bold  ">*</span>
+              </span>
+              <input
+                type="text"
+                class="form-control fs-4 p-3 rounded-4 border-0 "
+                name=""
+                id=""
+                aria-describedby="helpId"
+               
+              />
+            </div>
+            <div className="px-5 pt-5">
+              <span className='fs-4 text-secondary'>
+                نام کاربری <span className="text-danger fw-bold  ">*</span>
+              </span>
+              <input
+                type="text"
+                class="form-control fs-4 p-3 rounded-4 border-0 "
+                name=""
+                id=""
+                aria-describedby="helpId"
+               
+              />
+            </div>
+            <p>
+              اطلاعات شخصی شما برای پردازش سفارش شما استفاده می‌شود، و پشتیبانی
+              از تجربه شما در این وبسایت، و برای اهداف دیگری که در سیاست حفظ
+              حریم خصوصی توضیح داده شده است.
+            </p>
+            <a
+              class="btn btn-sm  btn-color fs-4 p-3 rounded-4"
+              href="#"
+              role="button"
+            >
+              عضویت
+            </a>
+          </div>
+        </div>
+        <div className="col m-4 ">
+          <span>عضویت</span>
+          <div
+            className="bg-light rounded-5 "
+          >
+            <div className="px-5 pt-5">
+              <span className='fs-4 text-secondary'>
+                نام کاربری <span className="text-danger fw-bold  ">*</span>
+              </span>
+              <input
+                type="text"
+                class="form-control fs-4 p-3 rounded-4 border-0 "
+                name=""
+                id=""
+                aria-describedby="helpId"
+               
+              />
+            </div>
+            <div className="px-5 pt-5">
+              <span className='fs-4 text-secondary'>
+                نام کاربری <span className="text-danger fw-bold  ">*</span>
+              </span>
+              <input
+                type="text"
+                class="form-control fs-4 p-3 rounded-4 border-0 "
+                name=""
+                id=""
+                aria-describedby="helpId"
+               
+              />
+            </div>
+            <div className="px-5 pt-5">
+              <span className='fs-4 text-secondary'>
+                نام کاربری <span className="text-danger fw-bold  ">*</span>
+              </span>
+              <input
+                type="text"
+                class="form-control fs-4 p-3 rounded-4 border-0 "
+                name=""
+                id=""
+                aria-describedby="helpId"
+               
+              />
+            </div>
+            <p>
+              اطلاعات شخصی شما برای پردازش سفارش شما استفاده می‌شود، و پشتیبانی
+              از تجربه شما در این وبسایت، و برای اهداف دیگری که در سیاست حفظ
+              حریم خصوصی توضیح داده شده است.
+            </p>
+            <a
+              class="btn btn-sm  btn-color fs-4 p-3 rounded-4"
+              href="#"
+              role="button"
+            >
+              عضویت
+            </a>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
