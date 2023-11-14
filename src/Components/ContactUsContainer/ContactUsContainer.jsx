810eded090366b48fce5eae4e9d7c8b51d58cdb4
m.hesam moradian <m.hesam.moradian@gmail.com>
2023-11-14 17:08:23 +0330

    .

--- src/Components/ContactUsContainer/ContactUsContainer.jsx
+++ src/Components/ContactUsContainer/ContactUsContainer.jsx
@@ -184,64 +184,10 @@ export default function ContactUsContainer() {
             <div class="card-body gap-3 d-grid justify-content-center jic ">
               <h5 class="card-title fs-3 fw-bold px-5">با ما تماس بگیرید</h5>
               <h6 class="card-subtitle mb-2 text-muted fs-5">
-                Info@yoursite.com
+                مدیریت: 03512345678
               </h6>
               <h6 class="card-subtitle mb-2 text-muted fs-5">
-                Info@yoursite.com
-              </h6>
-            </div>
-          </div>
-          <div class="ContactUsWaysToRichUsCard card col d-grid gap-4 p-5 border-0  align-items-center  shadow rounded-bottom-5  m-5">
-            <div className="d-grid justify-content-center ">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="32"
-                height="32"
-                viewBox="0 0 32 32"
-                fill="none"
-              >
-                <path
-                  d="M2.66528 11.3333C2.66528 6.66663 5.33195 4.66663 9.33195 4.66663H22.6653C26.6653 4.66663 29.332 6.66663 29.332 11.3333V20.6666C29.332 25.3333 26.6653 27.3333 22.6653 27.3333H9.33195"
-                  stroke="#0855DF"
-                  stroke-width="2"
-                  stroke-miterlimit="10"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                ></path>
-                <path
-                  d="M22.6681 12L18.4947 15.3333C17.1214 16.4267 14.868 16.4267 13.4947 15.3333L9.33472 12"
-                  stroke="#121212"
-                  stroke-width="2"
-                  stroke-miterlimit="10"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                ></path>
-                <path
-                  d="M2.66528 22H10.6653"
-                  stroke="#121212"
-                  stroke-width="2"
-                  stroke-miterlimit="10"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                ></path>
-                <path
-                  d="M2.66528 16.6666H6.66528"
-                  stroke="#121212"
-                  stroke-width="2"
-                  stroke-miterlimit="10"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                ></path>
-              </svg>
-            </div>
-
-            <div class="card-body gap-3 d-grid justify-content-center jic ">
-              <h5 class="card-title fs-3 fw-bold px-5">به ما ایمیل بزنید</h5>
-              <h6 class="card-subtitle mb-2 text-muted fs-5">
-                Info@yoursite.com
-              </h6>
-              <h6 class="card-subtitle mb-2 text-muted fs-5">
-                Info@yoursite.com
+                پشتیبانی: 03539876543
               </h6>
             </div>
           </div>
@@ -295,7 +241,7 @@ export default function ContactUsContainer() {
                 Info@yoursite.com
               </h6>
               <h6 class="card-subtitle mb-2 text-muted fs-5">
-                Info@yoursite.com
+                yourname@gmail.com
               </h6>
             </div>
           </div>
@@ -309,34 +255,48 @@ export default function ContactUsContainer() {
                 fill="none"
               >
                 <path
-                  d="M2.66528 11.3333C2.66528 6.66663 5.33195 4.66663 9.33195 4.66663H22.6653C26.6653 4.66663 29.332 6.66663 29.332 11.3333V20.6666C29.332 25.3333 26.6653 27.3333 22.6653 27.3333H9.33195"
+                  d="M15.9986 17.9067C18.2961 17.9067 20.1586 16.0442 20.1586 13.7467C20.1586 11.4492 18.2961 9.58667 15.9986 9.58667C13.7011 9.58667 11.8386 11.4492 11.8386 13.7467C11.8386 16.0442 13.7011 17.9067 15.9986 17.9067Z"
+                  stroke="#121212"
+                  stroke-width="1.9375"
+                ></path>
+                <path
+                  d="M4.8288 11.32C7.45547 -0.226704 24.5621 -0.21337 27.1755 11.3333C28.7088 18.1066 24.4955 23.84 20.8021 27.3866C18.1221 29.9733 13.8821 29.9733 11.1888 27.3866C7.5088 23.84 3.29547 18.0933 4.8288 11.32Z"
                   stroke="#0855DF"
-                  stroke-width="2"
-                  stroke-miterlimit="10"
+                  stroke-width="1.9375"
+                ></path>
+              </svg>
+            </div>
+
+            <div class="card-body gap-3 d-grid justify-content-center jic ">
+              <h5 class="card-title fs-3 fw-bold px-5">آدرس مجموعه ما</h5>
+              <h6 class="card-subtitle mb-2 text-muted fs-5">
+                تهران - میدان تجریش
+              </h6>
+              <h6 class="card-subtitle mb-2 text-muted fs-5">
+                یزد - میدان امیرچخماق
+              </h6>
+            </div>
+          </div>
+          <div class="ContactUsWaysToRichUsCard card col d-grid gap-4 p-5 border-0  align-items-center  shadow rounded-bottom-5  m-5">
+            <div className="d-grid justify-content-center ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 0 32 32"
+                fill="none"
+              >
+                <path
+                  d="M29.3334 16C29.3334 23.36 23.3601 29.3333 16.0001 29.3333C8.64008 29.3333 2.66675 23.36 2.66675 16C2.66675 8.63996 8.64008 2.66663 16.0001 2.66663C23.3601 2.66663 29.3334 8.63996 29.3334 16Z"
+                  stroke="#0855DF"
+                  stroke-width="2.0625"
                   stroke-linecap="round"
                   stroke-linejoin="round"
                 ></path>
                 <path
-                  d="M22.6681 12L18.4947 15.3333C17.1214 16.4267 14.868 16.4267 13.4947 15.3333L9.33472 12"
+                  d="M20.9451 20.24L16.8118 17.7733C16.0918 17.3466 15.5051 16.32 15.5051 15.48V10.0133"
                   stroke="#121212"
-                  stroke-width="2"
-                  stroke-miterlimit="10"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                ></path>
-                <path
-                  d="M2.66528 22H10.6653"
-                  stroke="#121212"
-                  stroke-width="2"
-                  stroke-miterlimit="10"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                ></path>
-                <path
-                  d="M2.66528 16.6666H6.66528"
-                  stroke="#121212"
-                  stroke-width="2"
-                  stroke-miterlimit="10"
+                  stroke-width="2.0625"
                   stroke-linecap="round"
                   stroke-linejoin="round"
                 ></path>
@@ -344,12 +304,12 @@ export default function ContactUsContainer() {
             </div>
 
             <div class="card-body gap-3 d-grid justify-content-center jic ">
-              <h5 class="card-title fs-3 fw-bold px-5">به ما ایمیل بزنید</h5>
+              <h5 class="card-title fs-3 fw-bold px-5">ساعات کاری</h5>
               <h6 class="card-subtitle mb-2 text-muted fs-5">
-                Info@yoursite.com
+                شنبه تا پنج شنبه
               </h6>
               <h6 class="card-subtitle mb-2 text-muted fs-5">
-                Info@yoursite.com
+                از 8 صبح الی 8 شب
               </h6>
             </div>
           </div>
