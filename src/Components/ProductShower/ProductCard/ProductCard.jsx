b490c0deef81d622791937332e8729f1e8ab0c32
m.hesam moradian <m.hesam.moradian@gmail.com>
2023-12-04 17:11:47 +0330

    .

--- src/Components/ProductShower/ProductCard/ProductCard.jsx
+++ src/Components/ProductShower/ProductCard/ProductCard.jsx
@@ -11,7 +11,6 @@ export const handleClick = (data) => {
     ? JSON.parse(localStorage.getItem("cards"))
     : [];
  
-  //     items.map((item) => {
   //       console.log(item.name + "==" + data.name);
   //       console.log(item.name == data.name);
   //       if (item.name == data.name) {
