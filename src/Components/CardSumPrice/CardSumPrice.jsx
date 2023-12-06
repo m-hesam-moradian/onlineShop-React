243e67bbfea671ff59ad1be08e7a3759cf8a2505
m.hesam moradian <m.hesam.moradian@gmail.com>
2023-12-06 14:32:44 +0330

    cardSumprice

---
+++ src/Components/CardSumPrice/CardSumPrice.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./CardSumPrice.css";
+
+export default function CardSumPrice() {
+  return (
+    <div>
+     
+    </div>
+  );
+}
--- src/pages/Card/Card.js
+++ src/pages/Card/Card.js
@@ -3,14 +3,24 @@ import "./Card.css";
 import Navbar from "../../Components/Navbar/Navbar";
 import Footer from "../../Components/Footer/Footer";
 import CardProductTable from "../../Components/CardProductTable/CardProductTable";
+import CardSumPrice from "../../Components/CardSumPrice/CardSumPrice";
 
 export default function Card() {
   return (
     <>
-          <Navbar />
-          <div className="p-5"></div>
-      <div className="col-7 ">
-       { JSON.parse(localStorage.getItem("cards"))&& <CardProductTable className=" m-5"></CardProductTable>}
+      <Navbar />
+      <div className="row m-0">
+        <div className="p-5"></div>
+        <div className="col-7 ">
+          {JSON.parse(localStorage.getItem("cards")) && (
+            <CardProductTable className="  m-5"></CardProductTable>
+          )}
+        </div>
+        <div className="col-3 ">
+          {JSON.parse(localStorage.getItem("cards")) && (
+            <CardSumPrice className="  m-5"></CardSumPrice>
+          )}
+        </div>
       </div>
       <Footer />
     </>
