c5b63b40e48d2b5b587e4cdb5dbf15c37664fd03
m.hesam moradian <m.hesam.moradian@gmail.com>
2023-12-25 21:07:23 +0330

    start again

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,21 +1,7 @@
-import React from "react";
-import { Button, Form, Nav, Navbar } from "react-bootstrap";
+import React from 'react'
 
 export default function Admin() {
   return (
-    <div>
-      <Navbar bg="dark" variant="dark " >
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-        <Form inline>
-          <Form.Control type="text" placeholder="Search" className="me-sm-2" />
-          <Button variant="outline-info">Search</Button>
-        </Form>
-      </Navbar>
-    </div>
-  );
+    <div>Admin</div>
+  )
 }
