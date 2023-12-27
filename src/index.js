656a47e09a1ea5947875b13480a2674b0790e8ad
m.hesam moradian <m.hesam.moradian@gmail.com>
2023-12-27 18:36:01 +0330

    start again claenn

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
 import App from "./App";
-
-import './styles/reset.css'
-import './styles/fonts.css'
-import './styles/variables.css'
-import './styles/helpers.css'
-import './styles/defaults.css'
+import "./index.css";
+import "./styles/reset.css";
+import "./styles/fonts.css";
+import "./styles/variables.css";
+import "./styles/helpers.css";
+import "./styles/defaults.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-   <BrowserRouter>
+  <BrowserRouter>
     <App />
-   </BrowserRouter>
+  </BrowserRouter>
   // </React.StrictMode>
 );
 
--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-
+import "./Admin.css";
 export default function Admin() {
   return (
-    <div>Admin</div>
-  )
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
