45db2574b98649fa957998ff60955ee0dd58a376
m.hesam moradian <m.hesam.moradian@gmail.com>
2023-12-29 20:21:12 +0330

    admin

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -3,7 +3,7 @@ import "./Admin.css";
 export default function Admin() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="">Hello world!</h1>
     </div>
   );
 }
