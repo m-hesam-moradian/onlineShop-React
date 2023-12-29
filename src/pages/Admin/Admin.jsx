a4df2f0625462be17405f4cedccd10562c070b36
m.hesam moradian <m.hesam.moradian@gmail.com>
2023-12-29 20:25:28 +0330

    make list

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -3,7 +3,9 @@ import "./Admin.css";
 export default function Admin() {
   return (
     <div className="admin">
-      <div className="admin-sidebar bg-stone-500">side bar</div>
+      <div className="admin-sidebar bg-stone-500">side bar
+      <ul></ul>
+      </div>
       <div className="admin-contant bg-stone-300">whole contant</div>
     </div>
   );
