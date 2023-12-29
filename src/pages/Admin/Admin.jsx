39a74fa254d90d9044f99663ec0ac48bc35ffa97
m.hesam moradian <m.hesam.moradian@gmail.com>
2023-12-29 20:24:50 +0330

    whole contant

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
 import "./Admin.css";
 export default function Admin() {
   return (
-    <div className='admin'>
-<div className='admin-sidebar bg-stone-500'>side bar</div>
+    <div className="admin">
+      <div className="admin-sidebar bg-stone-500">side bar</div>
+      <div className="admin-contant bg-stone-300">whole contant</div>
     </div>
   );
 }
