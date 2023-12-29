b8c58fc4a370caf9a2ce8abee5448ab21d26f63c
m.hesam moradian <m.hesam.moradian@gmail.com>
2023-12-29 20:25:53 +0330

    list items

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -4,7 +4,9 @@ export default function Admin() {
   return (
     <div className="admin">
       <div className="admin-sidebar bg-stone-500">side bar
-      <ul></ul>
+        <ul>
+          <li></li>
+      </ul>
       </div>
       <div className="admin-contant bg-stone-300">whole contant</div>
     </div>
