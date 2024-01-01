5da6616359cad73c7a8fe5462155d0d2980f945c
m.hesam moradian <m.hesam.moradian@gmail.com>
2024-01-01 17:34:44 +0330

    add admin side bar

--- src/pages/Admin/AdminSidebar/AdminSidebar.jsx
+++ src/pages/Admin/AdminSidebar/AdminSidebar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function AdminSidebar() {
   return (
     <div className="text-white text-[20px] ">
-      <div className="px-4 py-8 flex items-center justify-center border  border-stone-600 ">
+      <div className="px-4 py-8 flex items-center justify-center border ">
         <span className=" font-extrabold cursor-pointer leading-[24px] text-white ">
           sidebar
         </span>
