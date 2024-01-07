016a83806abd5a79d09b9b92abfe7900e5f31b9b
m.hesam moradian <m.hesam.moradian@gmail.com>
2024-01-07 20:35:29 +0330

    clean header component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Landing from "../Landing/Landing";
 import Navbar from "../Navbar/Navbar";
 import HeaderBottom from "../HeaderBottom/HeaderBottom";
@@ -6,19 +6,10 @@ import HeaderBottom from "../HeaderBottom/HeaderBottom";
 import "./Header.css";
 
 export default function Header() {
-  const [indexInfo, setIndexInfo] = useState({});
-
-  // useEffect(() => {
-  //   fetch('http://localhost:4000/v1/infos/index')
-  //   .then(res => res.json())
-  //   .then(allInfos => setIndexInfo(allInfos))
-  // }, [])
-
   return (
     <header className="header px-3 container-flud">
-      {/* <Topbar info={indexInfo} /> */}
       <Navbar />
-      <Landing  />
+      <Landing />
       <HeaderBottom />
     </header>
   );
