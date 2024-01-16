a7eb413a6991f001e02741e7591a8fe44589d199
m.hesam moradian <m.hesam.moradian@gmail.com>
2024-01-16 16:20:44 +0330

    done search inpute

--- src/Components/Admin/AdminNavbar.jsx
+++ src/Components/Admin/AdminNavbar.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export const AdminNavbar = () => {
   return (
-    <div className=''>AdminNavbar</div>
-  )
+    <div className="w-full bg-main-50 p-3 shadow">
+      <input
+        type="text"
+        placeholder="Search"
+        className=" border-2 rounded-full inputshadow shadow-div text-center text-main-400 "
+      />
+    </div>
+  );
 }
--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import AdminSidebar from "../../Components/Admin/AdminSidebar/AdminSidebar";
 import { Outlet } from "react-router-dom";
-// import "./Admin.css";
+import { AdminNavbar } from "../../Components/Admin/AdminNavbar";
+import "./Admin.css";
 export default function Admin() {
    window.scrollTo(0, 0);
   return (
     <div className="admin flex">
-      <div className="w-96  bg-gradient-to-r from-blue-900 to-black h-screen rounded-se-3xl rounded-ee-3xl">
+      <div className="w-96  bg-gradient-to-r from-blue-900 to-black h-screen ">
         <AdminSidebar />
       </div>
       <div className="basis-full   h-screen">
+        <AdminNavbar/>
         <Outlet />
       </div>
     </div>
