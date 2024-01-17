ccd2a268c97b1239acd7f04b8d5a61c26224e77d
m.hesam moradian <m.hesam.moradian@gmail.com>
2024-01-17 06:25:28 +0330

    mobile navbar full done

--- src/Components/Admin/AdminSidebar/AdminSidebar.jsx
+++ src/Components/Admin/AdminSidebar/AdminSidebar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function AdminSidebar() {
   return (
-    <div className="text-white flex items-center flex-col bg-[#e9edf2] h-full gap-3  ">
+    <div className="AdminSidebar transition-all duration-200 text-white flex items-center flex-col bg-[#e9edf2] h-full gap-3  ">
       <div className="pt-6 m-3  flex items-center justify-center  ">
-        <a href="" className=" flex  gap-2  text-white ">
+        <a href="" className=" flex  gap-2  text-white rounded-full ">
           <img
             className="w-full rounded-full  p-1 border-2 border-solid border-[#9696dd] "
             src="https://avatars.githubusercontent.com/u/137858394?s=400&u=c42efab78b203157feda7631149f5bc265bdafe5&v=4"
@@ -18,7 +19,7 @@ export default function AdminSidebar() {
       </div>
       <a href=" " className="text-main-75  btn3d rounded-full">
         {" "}
-        <div className=" m-3  text-[#9696dd]  text-2xl transition-all duration-300 ">
+        <div className=" m-3    text-2xl transition-all duration-300 ">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
@@ -35,9 +36,9 @@ export default function AdminSidebar() {
           {/* <span className="">صفحه اصلی</span> */}
         </div>{" "}
       </a>
-      <a href=" " className="text-main-75   rounded-full">
+      <NavLink to="/admin/" className="text-admin-text  rounded-full">
         {" "}
-        <div className=" m-3  text-[#9696dd]  text-2xl transition-all duration-300 ">
+        <div className=" m-3    text-2xl transition-all duration-300 ">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
@@ -47,13 +48,11 @@ export default function AdminSidebar() {
             <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
             <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
           </svg>
-
-          {/* <span className="">صفحه اصلی</span> */}
         </div>{" "}
-      </a>
-      <a href=" " className="text-main-75   rounded-full">
+      </NavLink>
+      <NavLink to="/admin/product" className="text-admin-text  rounded-full  ">
         {" "}
-        <div className=" m-3  text-[#9696dd]  text-2xl transition-all duration-300 ">
+        <div className=" m-3    text-2xl transition-all duration-300  ">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
@@ -67,10 +66,35 @@ export default function AdminSidebar() {
               clip-rule="evenodd"
             />
           </svg>
-
-          {/* <span className="">صفحه اصلی</span> */}
         </div>{" "}
-      </a>
+      </NavLink>
+      <NavLink to="/admin/message" className="text-admin-text  rounded-full">
+        {" "}
+        <div className=" m-3   text-2xl transition-all duration-300 ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            class="w-10"
+          >
+            <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
+            <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
+          </svg>
+        </div>{" "}
+      </NavLink>
+      <NavLink to="/admin/logout" className=" text-admin-text  rounded-full">
+        {" "}
+        <div className=" m-3    text-2xl transition-all duration-300 ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            class="w-10"
+          >
+            <path d="M10.375 2.25a4.125 4.125 0 1 0 0 8.25 4.125 4.125 0 0 0 0-8.25ZM10.375 12a7.125 7.125 0 0 0-7.124 7.247.75.75 0 0 0 .363.63 13.067 13.067 0 0 0 6.761 1.873c2.472 0 4.786-.684 6.76-1.873a.75.75 0 0 0 .364-.63l.001-.12v-.002A7.125 7.125 0 0 0 10.375 12ZM16 9.75a.75.75 0 0 0 0 1.5h6a.75.75 0 0 0 0-1.5h-6Z" />
+          </svg>
+        </div>{" "}
+      </NavLink>
     </div>
   );
 }
