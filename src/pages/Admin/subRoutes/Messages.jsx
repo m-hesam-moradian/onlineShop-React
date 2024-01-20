6c971acbc53de049a432555baf935b80c72a9092
m.hesam moradian <m.hesam.moradian@gmail.com>
2024-01-20 10:56:43 +0330

    show personal message done

--- src/pages/Admin/subRoutes/Messages.jsx
+++ src/pages/Admin/subRoutes/Messages.jsx
@@ -1,41 +1,43 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import adminContext from "../../../context/adminContext";
 import { useParams } from "react-router-dom";
 export const Messages = () => {
-  const[UserID,setUserId]=useState()
- const UseridParams  = useParams();
-   console.log(UseridParams.id);
-   const usetInfo = useContext(adminContext);
-   console.log(usetInfo.messagesData[UseridParams.id]);
+  
+  const [messageState, setMessageState] = useState();
+  const [NewMessage, setNewMessage] = useState();
+  const Userid = useParams();
+  console.log(Userid.id);
+  const usetInfo = useContext(adminContext);
 
+  useEffect(() => {
+    console.log(usetInfo && usetInfo.messagesData[Userid.id - 1].messages);
+    setMessageState(usetInfo && usetInfo.messagesData[Userid.id - 1].messages);
+  }, [Userid]);
   return (
-
     <div className=" h-full relative shadow-inner overflow-scroll pt-32 text-4xl">
       <nav className=" opacity-60  h-20 shadow bg-admin-text fixed top-0 w-full "></nav>
-      <div className="  flex px-5  justify-end w-full">
-        <p className="bg-white  rounded-t-3xl p-4 rounded-br-3xl w-full lg:w-1/3">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat rem
-          culpa numquam maxime dolore nisi officia repudiandae enim? Assumenda
-          voluptas asperiores corrupti neque facere facilis dolore enim incidunt
-          accusamus quod. Sapiente quidem perferendis quisquam animi accusantium
-          ducimus quos praesentium, at nobis nisi soluta? Maiores voluptas
-          accusantium eius sapiente necessitatibus ipsam eaque voluptates ut,
-          provident eligendi possimus perferendis explicabo aliquam minima!
-        </p>
-      </div>
-      <div className="  flex px-5 my-5   w-full">
-        <p className="  rounded-t-3xl p-4 bg-admin-text text-white rounded-bl-3xl w-full lg:w-1/3">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat rem
-          culpa numquam maxime dolore nisi officia repudiandae enim? Assumenda
-          voluptas asperiores corrupti neque facere facilis dolore enim incidunt
-          accusamus quod. Sapiente quidem perferendis quisquam animi accusantium
-          ducimus quos praesentium, at nobis nisi soluta? Maiores voluptas
-          accusantium eius sapiente necessitatibus ipsam eaque voluptates ut,
-          provident eligendi possimus perferendis explicabo aliquam minima!
-        </p>
-      </div>
 
-      <footer className="h-24  sticky bottom-0 w-full flex">
+      {messageState &&
+        messageState.map((item) => {
+          if (item.id) {
+            return (
+              <div className="  flex px-5  justify-end w-full">
+                <p className="bg-white  rounded-t-3xl p-4 rounded-br-3xl w-full lg:w-1/3">
+                  {item.content}
+                </p>
+              </div>
+            );
+          } else {
+            return (
+              <div className="  flex px-5 my-5   w-full">
+                <p className="  rounded-t-3xl p-4 bg-admin-text text-white rounded-bl-3xl w-full lg:w-1/3">
+                  {item.content}
+                </p>
+              </div>
+            );
+          }
+        })}
+      <footer className="h-24  fixed bottom-0 w-full flex">
         <a
           href=""
           className="m-3  bg-admin-hover shadow-lg text-admin-text  hover:bg-admin-active hover:text-admin-hover rounded-full flex items-center "
@@ -52,6 +54,10 @@ export const Messages = () => {
         <input
           type="text"
           className=" w-full m-3 shadow-lg rounded-full p-3 me-0"
+          onChange={() => {
+            
+          }}
+          value={NewMessage}
         />
       </footer>
     </div>
