739ebb36907842e2ebca8a25b5956b8d6fc211ce
m.hesam moradian <m.hesam.moradian@gmail.com>
2024-01-22 16:40:50 +0330

    .

--- src/pages/Admin/subRoutes/Product.jsx
+++ src/pages/Admin/subRoutes/Product.jsx
@@ -114,7 +114,7 @@ export const Product = () => {
                 </div>
 
                 
-                
+              
 
                 <div class="text-center flex">
                   <svg
