20c99b981e7ab56e0fa459707592e8cc0691d355
m.hesam moradian <m.hesam.moradian@gmail.com>
2024-01-22 11:27:43 +0330

    change the table for mor flexablity

--- src/pages/Admin/subRoutes/Product.jsx
+++ src/pages/Admin/subRoutes/Product.jsx
@@ -1,10 +1,26 @@
-import React from "react";
-import StickyHeadTable from "./ProductTable";
+import React, { useEffect, useState } from "react";
+import { API } from "../../../App";
+// import AdminTable from ". ";
 
 export const Product = () => {
-  return (
-    <div>
-      <StickyHeadTable />
-    </div>
-  );
+  const [tableData, setTableData] = useState();
+  useEffect(() => {
+    fetch(`${API}products`)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return res.json();
+      })
+      .then(async (allData) => {
+        setTableData(allData);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+    window.scrollTo(0, 0);
+  }, []);
+  console.log(tableData);
+
+  return <div>{/* <AdminTable/> */}</div>;
 };
