a51dad02a00e4df8d7f215150944e84c4e174deb
m.hesam moradian <m.hesam.moradian@gmail.com>
2024-01-27 12:19:18 +0330

    main chart import

--- src/pages/Admin/subRoutes/HomePage.jsx
+++ src/pages/Admin/subRoutes/HomePage.jsx
@@ -3,23 +3,30 @@ import NewUsersChart from "../../../Components/Admin/HomePage/NewUsersChart";
 import TotalExpenseChart from "../../../Components/Admin/HomePage/TotalExpenseChart";
 import TotalIcomeChart from "../../../Components/Admin/HomePage/TotalIcomeChart";
 import NewOrdersChart from "../../../Components/Admin/HomePage/NewOrdersChart";
+import MainChart from "../../../Components/Admin/HomePage/MainChart";
 
 export const HomePage = () => {
   return (
     <div>
       <div className="grid grid-cols-4">
-        <div className="bg-red-500 h-full">
+        <div className="bg-red-500 h-full rounded-xl shadow">
           <NewUsersChart />
         </div>
-        <div className="bg-yellow-500 h-full">
+        <div className="bg-yellow-500 h-full rounded-xl shadow">
           {" "}
           <TotalExpenseChart />{" "}
         </div>
-        <div className="bg-green-500 h-full">
+        <div className="bg-green-500 h-full rounded-xl shadow">
           <TotalIcomeChart />
         </div>
-        <div className="bg-blue-500 h-full">
+        <div className="bg-blue-500 h-full rounded-xl shadow">
           <NewOrdersChart />
+        </div>
+      </div>
+      <div className="grid grid-cols-2 ">
+        <div className="bg-white rounded-xl">
+          {" "}
+          <MainChart />
         </div>
       </div>
     </div>
