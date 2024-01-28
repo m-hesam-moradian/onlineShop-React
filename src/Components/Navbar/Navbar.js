37a4bd21980b1fbd199e72a523e31158dd03f897
m.hesam moradian <m.hesam.moradian@gmail.com>
2024-01-28 21:33:48 +0330

    edit navbar icon

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -92,7 +92,20 @@ export default function Navbar() {
                 e.preventDefault();
               }}
             >
-              <i className="fas fa-search "></i>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-10"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                />
+              </svg>
             </button>
 
             <ul className="searchList rounded-4 d-grid gap-3">
@@ -116,8 +129,14 @@ export default function Navbar() {
               } main-header__cart-btn contactBTN mobileNavCart d-flex d-md-none hover:scale-110`}
             >
               <span className="productCounter">{count}</span>
-
-              <i className="fas fa-shopping-cart main-header__cart-icon"></i>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class=" w-12"
+              >
+                <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
+              </svg>
             </NavLink>
             {authContext.isLoggedIn ? (
               <NavLink to="#" className="main-header__profile mainProfile">
@@ -154,8 +173,16 @@ export default function Navbar() {
           ) : (
             ""
           )}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            class=" w-12"
+          >
+            <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
+          </svg>
 
-          <i className="fas fa-shopping-cart main-header__cart-icon"></i>
+          {/* <i className="fas fa-shopping-cart main-header__cart-icon"></i> */}
         </NavLink>
       </div>
       <div className="navbarMenu d-md-flex ">
