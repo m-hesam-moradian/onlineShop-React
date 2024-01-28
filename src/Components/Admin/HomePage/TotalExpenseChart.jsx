030527a14df64bee46dce040e506aed4b8af81d8
m.hesam moradian <m.hesam.moradian@gmail.com>
2024-01-28 06:05:59 +0330

    remove the whole charts

--- src/Components/Admin/HomePage/TotalExpenseChart.jsx
+++ src/Components/Admin/HomePage/TotalExpenseChart.jsx
@@ -2,91 +2,39 @@ import React, { useState, useEffect } from "react";
 import ReactApexChart from "react-apexcharts";
 import ReactDOM from "react-dom";
 
-const TotalExpenseChart = () => {
-  const [chartOptions, setChartOptions] = useState({
-    chart: {
-    //   height: 350,
-      type: "line",
-      dropShadow: {
-        enabled: true,
-        color: "#000",
-        top: 18,
-        left: 7,
-        blur: 10,
-        opacity: 0.2,
-      },
-      toolbar: {
-        show: false,
-      },
-    },
-    colors: ["#77B6EA", "#545454"],
-    dataLabels: {
-      enabled: true,
-    },
-    stroke: {
-      curve: "smooth",
-    },
-    title: {
-      text: "Average High & Low Temperature",
-      align: "left",
-    },
-    grid: {
-      borderColor: "#e7e7e7",
-      row: {
-        colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
-        opacity: 0.5,
-      },
-    },
-    markers: {
-      size: 1,
-    },
-    xaxis: {
-      categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
-      title: {
-        text: "Month",
-      },
-    },
-    yaxis: {
-      title: {
-        text: "Temperature",
-      },
-      min: 5,
-      max: 40,
-    },
-    legend: {
-      position: "top",
-      horizontalAlign: "right",
-      floating: true,
-      offsetY: -25,
-      offsetX: -5,
-    },
-  });
 
-  const [chartSeries, setChartSeries] = useState([
+
+const TotalExpenseChart = () => {
+  const [series] = useState([
     {
-      name: "High - 2013",
-      data: [28, 29, 33, 36, 32, 32, 33],
-    },
-    {
-      name: "Low - 2013",
-      data: [12, 11, 14, 18, 17, 13, 13],
+      name: "Series 1",
+      data: [80, 50, 30, 40, 100, 20],
     },
   ]);
 
-  useEffect(() => {
-    const domContainer = document.querySelector("#chart2");
-    const apexChart = React.createElement(ReactApexChart, {
-      options: chartOptions,
-      series: chartSeries,
-      type: "line",
+  const [options] = useState({
+    chart: {
       height: 230,
-    });
-    ReactDOM.render(apexChart, domContainer);
-  }, [chartOptions, chartSeries]);
+      type: "radar",
+    },
+    title: {
+      text: "Basic Radar Chart",
+    },
+    xaxis: {
+      categories: ["January", "February", "March", "April", "May", "June"],
+    },
+  });
 
   return (
     <div>
-      <div id="chart2"></div>
+      <div id="chart">
+        <ReactApexChart
+          options={options}
+          series={series}
+          type="radar"
+          height={230}
+        />
+      </div>
       <div id="html-dist"></div>
     </div>
   );
--- src/pages/Admin/subRoutes/HomePage.jsx
+++ src/pages/Admin/subRoutes/HomePage.jsx
@@ -10,17 +10,27 @@ export const HomePage = () => {
     <div>
       <div className="grid grid-cols-4">
         <div className="bg-red-500 h-full rounded-xl shadow">
-          <NewUsersChart />
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="w-6 h-6"
+            >
+              <path d="M5.25 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM2.25 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM18.75 7.5a.75.75 0 0 0-1.5 0v2.25H15a.75.75 0 0 0 0 1.5h2.25v2.25a.75.75 0 0 0 1.5 0v-2.25H21a.75.75 0 0 0 0-1.5h-2.25V7.5Z" />
+            </svg>
+          </span>
+          {/* <NewUsersChart /> */}
         </div>
         <div className="bg-yellow-500 h-full rounded-xl shadow">
           {" "}
-          <TotalExpenseChart />{" "}
+          {/* <TotalExpenseChart />{" "} */}
         </div>
         <div className="bg-green-500 h-full rounded-xl shadow">
-          <TotalIcomeChart />
+          {/* <TotalIcomeChart /> */}
         </div>
         <div className="bg-blue-500 h-full rounded-xl shadow">
-          <NewOrdersChart />
+          {/* <NewOrdersChart /> */}
         </div>
       </div>
       <div className="grid grid-cols-2 ">
