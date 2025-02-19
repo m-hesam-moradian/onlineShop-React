96f70823d4c4ce242c72077e5978a55096470068
M.Hesam <m.hesam.moradian@gmail.com>
2025-02-19 08:32:56 +0330

    change again to browser router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import "./styles/reset.css";
@@ -12,8 +12,8 @@ import "./styles/defaults.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
   // </React.StrictMode>
 );
